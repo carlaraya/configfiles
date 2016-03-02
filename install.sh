d42e5b2ddb86041ade4d1f7042dea125a6578c25
carlaraya <carl_angelo.araya@upd.edu.ph>
2016-03-02 22:31:34 +0800

    Some more changes

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
 
 #vim
-ln -s .vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm -f ~/.vimrc
+ln -sf $(pwd)/.vimrc ~/.vimrc
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # git
-ln -s .gitconfig ~/.gitconfig
-ln -s .gitignore_global ~/.gitignore_global
+rm -f ~/.gitconfig
+rm -f ~/.gitignore_global
+ln -sf $(pwd)/.gitconfig ~/.gitconfig
+ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
